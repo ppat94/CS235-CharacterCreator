b0697f2a9456f5009bb967746254ed91914d9f18
Parth <parthpatel_1994@yahoo.co.uk>
2019-04-28 13:25:53 -0700

    Initial commit

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public Animator animator;
+
+    private int sceneToLoad;
+
+    public void OnBtnClick()
+    {
+        FadeToNextScene();
+    }
+
+    private void FadeToNextScene()
+    {
+        FadeToScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void FadeToScene(int sceneIndex)
+    {
+        sceneToLoad = sceneIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    private void OnFadeComplete()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
