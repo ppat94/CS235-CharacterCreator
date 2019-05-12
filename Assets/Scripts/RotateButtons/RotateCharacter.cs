62c3520ad860b0831aedad00de64619a3a521d31
Parth <parthpatel_1994@yahoo.co.uk>
2019-05-12 04:48:29 -0700

    Created script to Rotate character by dragging the gameobject in real-time.
    - Disabled the buttons on screen to rotate character left or right.

---
+++ Assets/Scripts/RotateButtons/RotateCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCharacter : MonoBehaviour
+{
+    public float angle;
+
+    void OnMouseDrag()
+    {
+        float x = Input.GetAxis("Mouse X");
+        transform.RotateAround(transform.position, new Vector3(0, 1, 0) * Time.deltaTime * -1 * x, angle);
+    }
+}
