4dfa67ce2e590e4b7b932ce9bb830cfd5a395602
Dennis <dang.dennis21@gmail.com>
2019-05-12 15:42:25 -0700

    attached hair and beard color scripts to buttons

---
+++ Assets/Scripts/CharacterCustomizer/Hair/BaseColorSlot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseColorSlot : MonoBehaviour {
+    public GameObject model;
+    public Image button;
+    protected Color buttonColor;
+    protected Transform trans;  // transform component of hair/beard
+
+    // match color with color of selected panel
+    public void Awake() {
+        buttonColor = button.transform.GetChild(0).GetComponent<Image>().color;
+        trans = model.transform.GetChild(0).GetChild(0);
+    }
+}
--- Assets/Scripts/CharacterCustomizer/Hair/BeardColorSlot.cs
+++ Assets/Scripts/CharacterCustomizer/Hair/BeardColorSlot.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HairColorSlot : BaseColorSlot {
+public class BeardColorSlot : BaseColorSlot {
     public new void Awake() {
         base.Awake();
     }
 
     public void OnClick() {
-        // select the hair renderer child game object
-        Transform t = trans.GetChild(0).GetChild(0);
+        // select the beard renderer child game object
+        Transform t = trans.GetChild(1).GetChild(0);
         t.GetComponent<Renderer>().material.color = buttonColor;
-
     }
 }
