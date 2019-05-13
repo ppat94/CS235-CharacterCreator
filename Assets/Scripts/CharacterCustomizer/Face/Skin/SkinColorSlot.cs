e7d591cec7d28f50ad435ecc82be134f097635c8
Parth <parthpatel_1994@yahoo.co.uk>
2019-05-13 15:54:55 -0700

    Modified the Character Customization scene.
    - Removed the eye color option. - Removed the color option for the clothing panel.

---
+++ Assets/Scripts/CharacterCustomizer/Face/Skin/SkinColorSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinColorSlot : BaseColorSlot
+{
+    public new void Awake()
+    {
+        base.Awake();
+    }
+
+    public void OnClick()
+    {
+        // select the hair renderer child game object
+        //Transform t = trans.GetChild(4);
+        //t.GetComponent<Renderer>().material.color = buttonColor;
+        model.transform.Find("challenger_head").GetComponent<Renderer>().material.color = buttonColor;
+        model.transform.Find("challenger_fingers").GetComponent<Renderer>().material.color = buttonColor;
+    }
+}
