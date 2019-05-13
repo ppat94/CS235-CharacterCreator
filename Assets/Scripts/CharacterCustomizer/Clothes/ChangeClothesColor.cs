e7af76ec964eb395fc54c4bc686a10771f883770
Dennis <dang.dennis21@gmail.com>
2019-05-12 22:07:36 -0700

    finished scripts for swapping out clothes

---
+++ Assets/Scripts/CharacterCustomizer/Clothes/ChangeClothesColor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeClothesColor : MonoBehaviour {
+    public GameObject clothing;
+
+    public void OnClick() {
+        Debug.Log(clothing.GetComponent<Renderer>().material.color);
+    }
+}
