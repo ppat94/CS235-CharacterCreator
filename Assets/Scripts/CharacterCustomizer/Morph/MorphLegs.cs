04762a627dee57d5703403d0634987043794f9fb
Parth <parthpatel_1994@yahoo.co.uk>
2019-05-13 01:59:02 -0700

    Added a morph shader and script to control the body structure of the character using slider.
    - Fixed the capsule collider for the character to rotate only when clicking and dragging on the character. BUG: When the character was zoomed in, the capsule collider would take size of the screen so clicking and dragging anywhere would move the character. This functionality clashes with just clicking on buttons and selecting props.

---
+++ Assets/Scripts/CharacterCustomizer/Morph/MorphLegs.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MorphLegs : MonoBehaviour
+{
+
+    public float extrusionAmount { get; set; }
+    public Renderer[] rend;
+    public GameObject[] gameObjects;
+
+    void Update()
+    {
+        // rend.material.SetFloat("_fooVal", shininess);//Set float assignes the var in the shader
+        //rend.material.SetFloat("_Amount", extrusionAmount);//Set float assignes the var in the shader
+
+        gameObjects = GameObject.FindGameObjectsWithTag("Legs");
+        foreach (GameObject go in gameObjects)
+        {
+            rend = go.GetComponentsInChildren<Renderer>();
+
+            foreach (Renderer r in rend)
+            {
+                foreach (Material mat in r.materials)
+                {
+                    mat.shader = Shader.Find("Example/Normal Extrusion");
+                    mat.SetFloat("_Amount", extrusionAmount);
+                }
+            }
+        }
+    }
+
+    //public float extrusionAmount { get; set; }
+    //public Renderer rend;
+
+    //void Start()
+    //{
+    //    rend = GetComponent<Renderer>();
+    //    rend.material.shader = Shader.Find("Example/Normal Extrusion");//Gets the Shader
+    //}
+    //void Update()
+    //{
+    //    // rend.material.SetFloat("_fooVal", shininess);//Set float assignes the var in the shader
+    //    rend.material.SetFloat("_Amount", extrusionAmount);//Set float assignes the var in the shader
+    //}
+}
