38538910d0662d117db3af2a7b785ec354375bb9
Parth <parthpatel_1994@yahoo.co.uk>
2019-05-14 03:49:01 -0700

    Added an Animations Manager for changing animations in realtime.
    - Added animations in the Resources folder for loading purposes. - Renamed various files and organized folder structures.

---
+++ Assets/Scripts/AnimationsManager/AnimationsManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationsManager : MonoBehaviour
+{
+
+    List<string> animationNames = new List<string>() { "Idle", "Walk", "T-Pose" };
+
+    public GameObject gameobject;
+    public Dropdown myDropdown;
+    protected Animator animator;
+    protected AnimatorOverrideController animatorOverrideController;
+
+    public void DropdownIndexChanged(int index)
+    {
+        switch(index)
+        {
+            case 0:
+                animator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load("Chal_Idle", typeof(RuntimeAnimatorController));
+                break;
+            case 1:
+                animator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load("Chal_Walk", typeof(RuntimeAnimatorController));
+                break;
+            case 2:
+                animator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load("Vol_ArmRaise", typeof(RuntimeAnimatorController));
+                break;
+            default:
+                animator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load("Chal_Idle", typeof(RuntimeAnimatorController));
+                break;
+
+        }
+    }
+
+    void Start()
+    {
+        PopulateList();
+        myDropdown = GetComponent<Dropdown>();
+        animator = gameobject.GetComponent<Animator>();
+        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = animatorOverrideController;
+    }   
+
+    void PopulateList()
+    {
+        myDropdown.AddOptions(animationNames);
+    }
+}
