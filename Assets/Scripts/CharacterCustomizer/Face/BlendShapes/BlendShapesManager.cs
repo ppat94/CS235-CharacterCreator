57bc9a37c3c626dac922fb67cfdcc1a03e7b7ee4
Parth <parthpatel_1994@yahoo.co.uk>
2019-05-14 20:30:21 -0700

    Added functionality for Blend Shapes to control facial expressions of the character.
    - Fixed character materials.

--- Assets/Scripts/CharacterCustomizer/Face/BlendShapes/BlendShapesManager.cs
+++ Assets/Scripts/CharacterCustomizer/Face/BlendShapes/BlendShapesManager.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BlendShapesManager : MonoBehaviour
 {
 
-    public float extrusionAmount { get; set; }
-    public Renderer[] rend;
     public SkinnedMeshRenderer skinnedMeshRenderer;
-    public GameObject go;
+    public GameObject blendShapeGameObject;
+    public Slider baseSlider;
+    public Slider lipsSlider;
+    public Slider scaredSlider;
+    public Slider angryUpperSlider;
+    public Slider angryLowerSlider;
 
-    void Update()
+    public void Update()
     {
-        go = GameObject.Find("challenger_head");
-        skinnedMeshRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
-        skinnedMeshRenderer.SetBlendShapeWeight(0, 100);
+        //Adds a listener to the main slider and invokes a method when the value changes.
+        baseSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        lipsSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        scaredSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        angryUpperSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+        angryLowerSlider.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+
+    // Invoked when the value of the slider changes.
+    public void ValueChangeCheck()
+    {
+        skinnedMeshRenderer = blendShapeGameObject.GetComponent<SkinnedMeshRenderer>();
+        skinnedMeshRenderer.SetBlendShapeWeight(0, baseSlider.value);
+        skinnedMeshRenderer.SetBlendShapeWeight(1, lipsSlider.value);
+        skinnedMeshRenderer.SetBlendShapeWeight(2, scaredSlider.value);
+        skinnedMeshRenderer.SetBlendShapeWeight(3, angryUpperSlider.value);
+        skinnedMeshRenderer.SetBlendShapeWeight(4, angryLowerSlider.value);
     }
 }
