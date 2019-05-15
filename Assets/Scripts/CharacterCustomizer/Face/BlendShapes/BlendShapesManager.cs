5870c1a72c911537dd6510d3297faddd85a3f001
Parth <parthpatel_1994@yahoo.co.uk>
2019-05-14 18:57:11 -0700

    Started working on Blend Shapes to control facial expressions of the character.

---
+++ Assets/Scripts/CharacterCustomizer/Face/BlendShapes/BlendShapesManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendShapesManager : MonoBehaviour
+{
+
+    public float extrusionAmount { get; set; }
+    public Renderer[] rend;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+    public GameObject go;
+
+    void Update()
+    {
+        go = GameObject.Find("challenger_head");
+        skinnedMeshRenderer = gameObject.GetComponent<SkinnedMeshRenderer>();
+        skinnedMeshRenderer.SetBlendShapeWeight(0, 100);
+    }
+}
